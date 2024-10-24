1df100ee3d0ce1c5c9c78a86b2ce583d623bb151
marufmustarmoon <marufmustarmoon@gmail.com>
2024-10-25 04:48:07 +0600

    basic changes

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,10 +1,23 @@
 const cors = require('cors');
 
+const allowedOrigins = [
+  'https://deploy-task-frontend.onrender.com',
+  'http://localhost:5173',
+  // Add more allowed origins here
+];
+
 const corsOptions = {
-  origin: 'https://deploy-task-frontend.onrender.com', 
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, 
-  optionsSuccessStatus: 204 
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // Allow requests with no Origin header
+
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 204,
 };
 
 module.exports = cors(corsOptions);
