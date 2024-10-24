d0a5fb3797f72556e4c37a0b29e8e763ef7575ae
marufmustarmoon <marufmustarmoon@gmail.com>
2024-10-25 04:44:38 +0600

    basic changes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -65,6 +65,7 @@ exports.login = async (req, res) => {
 
     res.status(200).json({ token });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: 'Server error' });
   }
 };
